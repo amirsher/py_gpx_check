8b9c5eeabbbb00f0bef881d51a754826d2794cf5
Amir Sher <locosher@gmail.com>
2017-12-27 23:43:45 +0200

    add simple Tkinter gui

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 from tkinter import *
-from subprocess import call, Popen, PIPE
+from subprocess import check_output, Popen, PIPE
 from tkinter import messagebox
+import webbrowser, os
 
 top = Tk()
 top.title("Check gpx files")
@@ -10,19 +11,41 @@ top.title("Check gpx files")
 
 def speeding():
     B2.flash()
-    p = Popen("python speeding.py " + E2.get(), shell=True, stdout=PIPE)
-    output = p.communicate()
-    text.insert(END, output)
+    p = Popen("python speeding.py " + E2.get(), stdout=PIPE, shell=True, universal_newlines=True)
+    while True:
+        retcode = p.poll()
+        output = p.stdout.readline()
+        print(output)
+        text.insert(END, output)
+        if retcode is not None:
+            break
+    text.insert(END, "\n")
 
-    messagebox.showinfo("Information","Finished checking speeding")
+    filename = "SpeedingMap.html"
+    result = messagebox.askquestion("Results", "Show results in web browser?")
+    if result == 'yes':
+        webbrowser.open('file://' + os.path.realpath(filename))
+    else:
+        messagebox.showinfo("Information","saved results to " + filename)
     
 def marshaling():
     B1.flash()
-    p = Popen("python marshaling.py " + E1.get(), shell=True, stdout=PIPE)
-    output = p.communicate()
-    text.insert(END, output)
+    p = Popen("python marshaling.py " + E1.get(), stdout=PIPE, shell=True, universal_newlines=True)
+    while True:
+        retcode = p.poll()
+        output = p.stdout.readline()
+        print(output)
+        text.insert(END, output)
+        if retcode is not None:
+            break
+    text.insert(END, "\n")
 
-    messagebox.showinfo("Information","Finished checking marshaling")
+    filename = "TrackingMap.html"
+    result = messagebox.askquestion("Results", "Show results in web browser?")
+    if result == 'yes':
+        webbrowser.open('file://' + os.path.realpath(filename))
+    else:
+        messagebox.showinfo("Information","saved results to " + filename)
 
 E2 = Entry(top, bd =5, width=100, font=("Ariel", "28"))
 E2.pack()
